7d0ccc312f32a6c19b9536694f4b87133fe08f5f
jychoi-git <jychoinv@gmail.com>
2024-12-07 22:17:38 +0900

    calc.py 연산 선택기능 추가

--- calc.py
+++ calc.py
@@ -1,4 +1,14 @@
+print("연산 종류")
+print(" 1: 더하기")
+print(" 2: 빼기")
+print(" 3: 곱하기")
+print(" 4: 나누기")
 def main():
-pass
+    while True:
+        choice = input("(1/2/3/4) 중에서 선택하세요: ")
+        if choice in ('1', '2', '3', '4'):
+            pass
+        else:
+            break
 if __name__ == "__main__":
-main()
+    main()
